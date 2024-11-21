162a571d2948c3c0a378a7a08df175de2d9946de
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-21 15:00:59 +0500

    Improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Carousel from "../components/carousel";
-
-// import CarouselTwo from "@/components/carouselTwo";
+import CarouselTwo from "@/components/carouselTwo";
 
 export default function Home() {
   return (
+    <>
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <Carousel />
-      {/* <CarouselTwo /> */}
     </div>
+    <div>
+      <CarouselTwo />
+    </div>
+    </>
   )
 }
--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -52,17 +52,19 @@ export default function Carousel() {
             className="flex-shrink-0 mx-12"
             style={{ width: "900px", height: "550px" }}
           >
-            <div className="relative w-full h-full rounded-lg overflow-hidden shadow-lg group">
+            <div className="relative w-full h-full rounded-lg overflow-hidden shadow-lg group hover:bg-green-200 transition-all duration-500">
+              {/* Image with dimming effect on hover */}
               <Image
                 src={slide.image}
                 alt={slide.title}
                 fill
-                className="object-cover"
+                className="object-cover transition-all duration-500 group-hover:opacity-50"
               />
-              <div className="absolute bottom-5 inset-x-0 bg-opacity-50 flex flex-col justify-center items-center text-white">
+              {/* Content container */}
+              <div className="absolute bottom-5 inset-x-0 bg-opacity-50 flex flex-col justify-center items-center text-white duration-300  group-hover:opacity-100 transition-transform transform group-hover:translate-y-[-40px] translate-y-10">
                 <h2 className="text-2xl font-bold">{slide.title}</h2>
-                <p className="mt-2 text-center">{slide.description}</p>
-                <p className="transition-transform transform group-hover:translate-y-0 translate-y-10 opacity-0 group-hover:opacity-100">
+                <p className="mt-2 text-center mb-3">{slide.description}</p>
+                <p className=" opacity-0 group-hover:opacity-100">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Ducimus sint deleniti minus illo cupiditate dolore mollitia.
                 </p>
@@ -70,7 +72,10 @@ export default function Carousel() {
             </div>
           </div>
         ))}
-        <button
+      </div>
+
+      {/* Navigation Buttons */}
+      <button
         onClick={handlePrev}
         className="bg-gray-800 absolute top-1/2 left-0 transform -translate-y-1/2 text-white p-3 rounded-full shadow-lg hover:bg-gray-700 z-10"
       >
@@ -82,10 +87,6 @@ export default function Carousel() {
       >
         →
       </button>
-      </div>
-
-      {/* Navigation Buttons Outside of the Carousel */}
-      
     </div>
   );
 }
